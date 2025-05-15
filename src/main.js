16279195207aa6f600ff8bc9abbdcdd8e85c35e1
Kamila <kamillkaa95@gmail.com>
2025-05-16 00:43:11 +0300

    hw

---
+++ src/main.js
@@ -0,0 +1,39 @@
+import { getImagesByQuery } from './js/pixabay-api.js';
+import { createGalleryMarkup, clearGallery } from './js/render-functions.js';
+import { showLoader, hideLoader } from './js/loader.js';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.search-form');
+const gallery = document.querySelector('.gallery');
+
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const formData = new FormData(form);
+const query = formData.get('searchQuery')?.trim();
+
+  if (!query) {
+    iziToast.warning({ title: 'Warning', message: 'Enter a search term' });
+    return;
+  }
+
+    
+    clearGallery();
+  showLoader();
+
+  try {
+      const images = await getImagesByQuery(query);
+      
+console.log('IMAGES:', images);
+    if (images.length === 0) {
+      iziToast.info({ title: 'Info', message: 'No images found' });
+      return;
+    }
+    createGalleryMarkup(images);
+  } catch (error) {
+    iziToast.error({ title: 'Error', message: 'Something went wrong' });
+    console.error(error);
+  } finally {
+    hideLoader();
+  }
+});
